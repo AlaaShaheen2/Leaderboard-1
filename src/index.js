3d31fc5af8f48a35af7bf886dee0d57b6f57c09c
inakiesk <inakieskruiz@gmail.com>
2022-09-09 08:15:22 -0500

    Add API display module with key generator module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import displayList from './modules/displayList.js';
+import id from './modules/gameID.js';
+
+const postUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+const submit = document.querySelector('.submit');
+const refresh = document.querySelector('.refresh');
+const user = document.querySelector('.add-name');
+const score = document.querySelector('.add-score');
---
+++ src/modules/displayList.js
@@ -0,0 +1,26 @@
+import id from './gameID';
+
+const postUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games';
+
+class displayList {
+  static getData = async () => {
+    const response = await fetch(`${postUrl}/${id}/scores`);
+    const info = await response.json();
+    const infoResult = info.result;
+    infoResult.forEach((data) => {
+      displayList.render(data);
+    });
+  };
+
+  static render(data) {
+    const scoresBoard = document.querySelector('.scores-board')
+    const scoreInfo = document.createElement('li');
+    scoreInfo.innerHTML = `
+      <p>${data.user}</p>
+      <p>${data.score}</p>
+      `;
+
+    scoresBoard.appendChild(scoreInfo);
+  }
+}
+export default displayList;
